a9fec1dc2cfb6a81b7a7750a38c900b6f38f2122
ivan <tellezivan7@gmail.com>
2023-07-10 08:55:23 -0700

    Initial Commit

---
+++ MovieFanatic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieFanatic
+//
+//  Created by Telles Ivan on 7/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieFanatic/MovieFanaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFanaticApp.swift
+//  MovieFanatic
+//
+//  Created by Telles Ivan on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieFanaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
